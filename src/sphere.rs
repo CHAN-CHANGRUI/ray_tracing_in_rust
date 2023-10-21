2f7ee4ed5c72c44fe60d8623aa5b3f71cf8c27ed
CHAN-CHANGRUI <powctor@outlook.com>
2023-10-21 14:42:52 +0800

    modified:   src/sphere.rs

--- src/sphere.rs
+++ src/sphere.rs
@@ -22,9 +22,9 @@ impl <Material>Sphere<Material> {
 
 impl <Material: Sync + material::Material>Hittable for Sphere <Material> {
     fn hit(&self, r: &Ray, ray_t: &Interval) -> Option<HitRecord> {
-        let oc = self.center - r.origin();
+        let oc = r.origin() - self.center;
         let a = r.direction().length_squared();
-        let half_b = Vec3::dot(r.direction(), oc);
+        let half_b = Vec3::dot(oc, r.direction());
         let c = oc.length_squared() - self.radius * self.radius;
 
         let discriminant = half_b * half_b - a * c;
@@ -33,9 +33,9 @@ impl <Material: Sync + material::Material>Hittable for Sphere <Material> {
         }
         let sqrtd = discriminant.sqrt();
 
-        let mut root = (half_b - sqrtd) / a;
+        let mut root = -(half_b + sqrtd) / a;
         if !ray_t.surrounds(root) {
-            root = (half_b + sqrtd) / a;
+            root = (-half_b + sqrtd) / a;
             if !ray_t.surrounds(root) {
                 return None;
             }
